1611ff32e108a29fd5c760bf4c336c7303117d8d
Leonardo Rodrigues Lima <lrodlima@gmail.com>
2020-12-08 11:36:32 -0300

    Creating a Service for Fraud Detection

--- src/main/java/br/com/cleancoding/ecommerce/NewOrderMain.java
+++ src/main/java/br/com/cleancoding/ecommerce/NewOrderMain.java
@@ -1,7 +1,6 @@
 package br.com.cleancoding.ecommerce;
 
 import org.apache.kafka.clients.producer.KafkaProducer;
-import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.serialization.StringSerializer;
@@ -14,7 +13,13 @@ public class NewOrderMain {
     public static void main(String[] args) throws ExecutionException, InterruptedException {
         var producer = new KafkaProducer<String, String>(properties());
         var record = new ProducerRecord<>("ECOMMERCE_NEW_ORDER", "order0", "900");
-        producer.send(record).get();
+        producer.send(record, (data, ex) -> {
+            if(ex != null){
+                ex.printStackTrace();
+                return;
+            }
+            System.out.println("Sucesso enviando: " + data.topic() + "::: partition " + data.partition() + "/ offset " + data.offset() + "/ timestamp " + data.timestamp());
+        }).get();
     }
 
     private static Properties properties() {
