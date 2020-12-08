222ff28b4f86ad2862b5c050923595271c7aa781
Leonardo Rodrigues Lima <lrodlima@gmail.com>
2020-12-07 23:55:15 -0300

    Adding .gitignore and NewOrderMain class

---
+++ src/main/java/br/com/cleancoding/ecommerce/NewOrderMain.java
@@ -0,0 +1,27 @@
+package br.com.cleancoding.ecommerce;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+import java.util.concurrent.ExecutionException;
+
+public class NewOrderMain {
+
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        var producer = new KafkaProducer<String, String>(properties());
+        var record = new ProducerRecord<>("ECOMMERCE_NEW_ORDER", "order0", "900");
+        producer.send(record).get();
+    }
+
+    private static Properties properties() {
+        var properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        return properties;
+    }
+}
